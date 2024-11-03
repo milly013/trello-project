48f57d364951cc1e29624d765c3d8e9a44346fc1
Milijan Kovac <milijankovac013@gmail.com>
2024-11-03 15:41:57 +0100

    Dodavanje project-servisa i povezivanje sa bazom

---
+++ back/project-service/main.go
@@ -0,0 +1,70 @@
+// main.go
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var projectCollection *mongo.Collection
+
+func main() {
+	// Povezivanje na MongoDB
+	client, err := connectToMongoDB()
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	// Referenca na kolekciju
+	projectCollection = client.Database("mydatabase").Collection("users")
+
+	router := gin.Default()
+
+	// API ruta za korisnike
+	router.GET("/users", getUsers)
+
+	router.Run(":8080")
+}
+
+// Funkcija za povezivanje na MongoDB
+func connectToMongoDB() (*mongo.Client, error) {
+	// Opcije konekcije
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Kreiramo novi MongoDB klijent
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Povezujemo se na MongoDB server
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Testiramo konekciju
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client, nil
+}
+
+// Handler za GET /users
+func getUsers(c *gin.Context) {
+	// Dummy podaci za testiranje, kasnije ćemo ih zameniti podacima iz baze
+	c.JSON(200, gin.H{"message": "Retrieve users from MongoDB here"})
+}
