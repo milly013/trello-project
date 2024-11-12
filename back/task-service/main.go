be0fcbbab0fcde84c19e24d5bd04f7ef25a3ac89
Milijan Kovac <milijankovac013@gmail.com>
2024-11-13 00:57:09 +0100

    Omoguceno pokretanje docker kontenjera

--- back/task-service/main.go
+++ back/task-service/main.go
@@ -5,20 +5,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/handlers"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 	"github.com/milly013/trello-project/back/task-service/handler"
 	"github.com/milly013/trello-project/back/task-service/repository"
 	"github.com/milly013/trello-project/back/task-service/service"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var taskCollection *mongo.Collection
 
 func main() {
+	// Učitajte .env fajl
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// Povezivanje na MongoDB
 	client, err := connectToMongoDB()
 	if err != nil {
@@ -26,45 +34,52 @@ func main() {
 	}
 	defer client.Disconnect(context.TODO())
 
-	// Referenca na kolekciju
-	taskCollection = client.Database("mydatabase").Collection("tasks")
-
-	router := gin.Default()
+	// Kreiramo instancu baze podataka
+	db := client.Database("mydatabase")
+	taskCollection = db.Collection("tasks")
 
 	taskRepo := repository.NewTaskRepository(taskCollection)
 	taskService := service.NewTaskService(taskRepo)
 	taskHandler := handler.NewTaskHandler(taskService)
+
+	router := gin.Default()
 
 	// API rute za zadatke
 	router.POST("/tasks", taskHandler.CreateTask)
 	router.GET("/tasks", taskHandler.GetTasks)
 	router.PUT("/tasks/:id", taskHandler.UpdateTask)
 	router.GET("/tasks/:id", taskHandler.GetTaskById)
-	//router.DELETE("/tasks/:id", taskHandler.DeleteTask)
 	router.POST("/tasks/add-member", taskHandler.AssignMemberToTask)
 
-	router.Run(":8082")
-
+	// Konfiguracija CORS-a
 	corsHandler := handlers.CORS(
-		handlers.AllowedOrigins([]string{"http://localhost:4200"}), // Set the correct origin
+		handlers.AllowedOrigins([]string{os.Getenv("CORS_ALLOWED_ORIGINS")}),
 		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"}),
 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 	)
 
-	srv := &http.Server{
-
-		Handler: corsHandler(router),
-		Addr:    ":8082",
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8082" // Default port
 	}
 
-	log.Println("Server is running on port 8082")
-	log.Fatal(srv.ListenAndServe())
+	srv := &http.Server{
+		Handler: corsHandler(router),
+		Addr:    ":" + port,
+	}
 
+	log.Println("Server is running on port " + port)
+	log.Fatal(srv.ListenAndServe())
 }
 
 // Funkcija za povezivanje na MongoDB
 func connectToMongoDB() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGO_URI environment variable not set")
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
 		return nil, err
