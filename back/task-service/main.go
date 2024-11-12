a93841269e77d390e876e29b6c00dce4eef5ea05
nikoladjurein <nikdjurein@gmail.com>
2024-11-12 17:37:30 +0100

    omoguceno dodavanje korisnika na taks

--- back/task-service/main.go
+++ back/task-service/main.go
@@ -9,8 +9,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/handlers"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/milly013/trello-project/back/task-service/handler"
+	"github.com/milly013/trello-project/back/task-service/repository"
+	"github.com/milly013/trello-project/back/task-service/service"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,13 +31,19 @@ func main() {
 
 	router := gin.Default()
 
-	// API rute za zadatke
-	router.POST("/tasks", createTask)
-	router.GET("/tasks", getTasks)
-	router.PUT("/tasks/:id", updateTask)
-	router.DELETE("/tasks/:id", deleteTask)
+	taskRepo := repository.NewTaskRepository(taskCollection)
+	taskService := service.NewTaskService(taskRepo)
+	taskHandler := handler.NewTaskHandler(taskService)
 
-	//router.Run(":8082")
+	// API rute za zadatke
+	router.POST("/tasks", taskHandler.CreateTask)
+	router.GET("/tasks", taskHandler.GetTasks)
+	router.PUT("/tasks/:id", taskHandler.UpdateTask)
+	router.GET("/tasks/:id", taskHandler.GetTaskById)
+	//router.DELETE("/tasks/:id", taskHandler.DeleteTask)
+	router.POST("/tasks/add-member", taskHandler.AssignMemberToTask)
+
+	router.Run(":8082")
 
 	corsHandler := handlers.CORS(
 		handlers.AllowedOrigins([]string{"http://localhost:4200"}), // Set the correct origin
@@ -78,92 +85,4 @@ func connectToMongoDB() (*mongo.Client, error) {
 
 	fmt.Println("Connected to MongoDB!")
 	return client, nil
-}
-
-// Struktura za zadatak
-type Task struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Title       string             `bson:"title" json:"title"`
-	Description string             `bson:"description" json:"description"`
-	Status      string             `bson:"status" json:"status"`
-	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`
-}
-
-// Handler za kreiranje novog zadatka
-func createTask(c *gin.Context) {
-	var task Task
-	if err := c.ShouldBindJSON(&task); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
-		return
-	}
-	task.CreatedAt = time.Now()
-
-	result, err := taskCollection.InsertOne(context.TODO(), task)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(201, gin.H{"taskId": result.InsertedID})
-}
-
-// Handler za dohvatanje svih zadataka
-func getTasks(c *gin.Context) {
-	cursor, err := taskCollection.Find(context.TODO(), bson.M{})
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-	defer cursor.Close(context.TODO())
-
-	var tasks []Task
-	if err = cursor.All(context.TODO(), &tasks); err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(200, tasks)
-}
-
-// Handler za ažuriranje zadatka
-func updateTask(c *gin.Context) {
-	idParam := c.Param("id")
-	id, err := primitive.ObjectIDFromHex(idParam)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid task ID"})
-		return
-	}
-
-	var task Task
-	if err := c.ShouldBindJSON(&task); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
-		return
-	}
-
-	update := bson.M{"$set": task}
-	_, err = taskCollection.UpdateByID(context.TODO(), id, update)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(200, gin.H{"message": "Task updated"})
-}
-
-// Handler za brisanje zadatka
-func deleteTask(c *gin.Context) {
-	idParam := c.Param("id")
-	id, err := primitive.ObjectIDFromHex(idParam)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid task ID"})
-		return
-	}
-
-	_, err = taskCollection.DeleteOne(context.TODO(), bson.M{"_id": id})
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(200, gin.H{"message": "Task deleted"})
 }
--- back/task-service/model/task.go
+++ back/task-service/model/task.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Task struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`        // ID zadatka
-	ProjectID   primitive.ObjectID `bson:"projectId" json:"projectId"`     // ID projekta kojem pripada
-	Title       string             `bson:"title" json:"title"`             // Naziv zadatka
-	Description string             `bson:"description" json:"description"` // Opis zadatka
-	StartDate   time.Time          `bson:"startDate" json:"startDate"`     // Datum početka zadatka
-	EndDate     time.Time          `bson:"endDate" json:"endDate"`         // Datum završetka zadatka
-	AssignedTo  primitive.ObjectID `bson:"assignedTo" json:"assignedTo"`   // ID korisnika kojem je zadatak dodeljen
-	Status      string             `bson:"status" json:"status"`           // Status zadatka (npr. "Pending", "InProgress", "Completed")
-	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`     // Datum kreiranja zadatka
+	ID          primitive.ObjectID   `bson:"_id,omitempty" json:"id"`        // ID zadatka
+	ProjectID   primitive.ObjectID   `bson:"projectId" json:"projectId"`     // ID projekta kojem pripada
+	Title       string               `bson:"title" json:"title"`             // Naziv zadatka
+	Description string               `bson:"description" json:"description"` // Opis zadatka
+	StartDate   time.Time            `bson:"startDate" json:"startDate"`     // Datum početka zadatka
+	EndDate     time.Time            `bson:"endDate" json:"endDate"`         // Datum završetka zadatka
+	AssignedTo  []primitive.ObjectID `bson:"assignedTo" json:"assignedTo"`   // Lista ID-ova korisnika kojima je zadatak dodeljen
+	Status      string               `bson:"status" json:"status"`           // Status zadatka (npr. "Pending", "InProgress", "Completed")
+	CreatedAt   time.Time            `bson:"createdAt" json:"createdAt"`     // Datum kreiranja zadatka
 }
