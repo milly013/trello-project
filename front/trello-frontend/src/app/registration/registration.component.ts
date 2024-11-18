e7d36b2b74500284ad78ec8def6ec31e41ac9ae2
Milijan Kovac <milijankovac013@gmail.com>
2024-11-18 17:58:05 +0100

    Sanitizacija unosa korisnika

--- front/trello-frontend/src/app/registration/registration.component.ts
+++ front/trello-frontend/src/app/registration/registration.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http'; // Importuj HttpClient
+import { HttpClient } from '@angular/common/http'; 
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { DomSanitizer, SafeValue } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule] 
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class RegistrationComponent {
   registrationForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router, private sanitizer: DomSanitizer) {
     this.registrationForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
@@ -31,15 +32,15 @@ export class RegistrationComponent {
 
     const formData = this.registrationForm.value;
 
-    const user = {
-      username: formData.username,
-      email: formData.email,
-      password: formData.password,
+    // Sanitizacija unosa korisnika kako bi se izbjegao XSS napad
+    const sanitizedUser = {
+      username: this.sanitizeInput(formData.username),
+      email: this.sanitizeInput(formData.email),
+      password: this.sanitizeInput(formData.password),
       role: formData.isManager ? 'manager' : 'member'
     };
 
-    // Pošaljemo HTTP POST zahtev na /users
-    this.http.post('http://localhost:8080/users', user)
+    this.http.post('http://localhost:8080/users', sanitizedUser)
       .subscribe({
         next: (response) => {
           console.log('Korisnik je uspešno registrovan', response);
@@ -51,6 +52,13 @@ export class RegistrationComponent {
           alert('Došlo je do greške prilikom registracije.');
         }
       });
+  }
+
+  sanitizeInput(input: string): string {
+    // Escape-ovanje opasnih znakova kako bi se spriječili XSS napadi
+    const element: HTMLElement = document.createElement('div');
+    element.innerText = input;
+    return element.innerHTML;
   }
 
   get f() {
